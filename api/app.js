86fce50e25d03ee130a035a3489c468b325b4a37
Tom Nudd <thomasgnudd@gmail.com>
2020-07-11 14:41:40 +0100

    integrate mongodb

--- api/app.js
+++ api/app.js
@@ -5,28 +5,51 @@ const fetch = require("node-fetch");
 const {URLSearchParams} = require("url");
 
 /*
-    User schema
+    Environment variables
+*/
+
+const config = require("dotenv").config();
+ 
+if (config.error) {
+    console.log("No .env file found!")
+}
+
+/*
+    Database setup
 */
 
 const mongoose = require("mongoose"),
     Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+    ObjectId = mongoose.Types.ObjectId;
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to database");
+});
+
+/*
+    User schema
+*/
 
 const userSchema = new Schema({
     _id: String,
     access_token: String,
-    token_expiry: {type: Date, default: Date.now},
+    token_expiry: {type: Date, default: Date.now()},
     refresh_token: String,
     displayName: String,
     url: String,
 })
 
+// Hunting for user accounts!
 userSchema.statics.findOrCreate = function findOrCreate(profile, callback) {
-    console.log(profile)
     let userObj = new this();
 
-    this.findOne({_id: profile.id}, function(err, res) { 
+    this.findOne({_id: profile.id}, function(err, res) {
         if (!res) {
+            console.log(profile.id, "not found")
             let dateObj = new Date();
             dateObj.setSeconds(dateObj.getSeconds() + parseInt(profile.expires_in));
 
@@ -38,22 +61,13 @@ userSchema.statics.findOrCreate = function findOrCreate(profile, callback) {
             userObj.token_expiry = dateObj;
             userObj.save(callback);
         } else {
+            console.log("found")
             callback(err, res);
         }
     });
 };
 
-
-const User = mongoose.model("User", userSchema);
-
-/*
-    Environment variables
-*/
-const config = require("dotenv").config();
- 
-if (config.error) {
-    console.log("No .env file found!")
-}
+const User = db.model("User", userSchema);
 
 /*
     Spotify calls
@@ -68,6 +82,7 @@ user-read-currently-playing playlist-read-collaborative \
 playlist-modify-public playlist-read-private \
 playlist-modify-private"
 
+// login route
 app.get("/login", (req, res) => {
     res.redirect("https://accounts.spotify.com/authorize" + 
     '?response_type=code' +
@@ -76,14 +91,13 @@ app.get("/login", (req, res) => {
     '&redirect_uri=' + encodeURIComponent(SPOTIFY_REDIRECT_URI));
 })
 
-
+// callback for spotify
 app.get("/login/callback", async (req, res) => {
     if (req.query.error) {
         // temp solution until FE set up
         console.log(req.query.error);
         res.redirect("/login");
     } else if (req.query && req.query.code) {
-        console.log(req)
         const code = req.query.code;
 
         const parameters = new URLSearchParams();
@@ -107,12 +121,15 @@ app.get("/login/callback", async (req, res) => {
             },
         }).then(response => response.json());
 
+        // combine user data with token data
         combinedData = Object.assign({}, userData, rawToken);
 
-        console.log();
         res.redirect("/")
 
-        User.findOrCreate(combinedData);
+        User.findOrCreate(combinedData, (err, result) => {
+            console.log("Error", err);
+            console.log("Result", result)
+        });
     }
 })
 
